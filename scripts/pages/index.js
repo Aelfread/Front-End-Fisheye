d65f039e6cb5c5163a1dd8b5139b1fabdbeefb47
Alfred <alfred.tjd@hotmail.com>
2024-04-24 15:05:02 +0400

    Intégrer la page d'accueil terminée

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,7 +1,11 @@
-    async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
+async function getPhotographers() {
+  // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet,
+  // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
+  const response = await fetch("data/photographers.json");
+  const data = await response.json();
+  const { photographers } = data;
+  console.log(photographers);
+  /*let photographers = [
             {
                 "name": "Ma data test",
                 "id": 1,
@@ -20,27 +24,27 @@
                 "price": 500,
                 "portrait": "account.png"
             },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
-    }
+        ]*/
+  // et bien retourner le tableau photographers seulement une fois récupéré
+  return {
+    photographers: [...photographers /*, ...photographers, ...photographers*/],
+  };
+}
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+async function displayData(photographers) {
+  const photographersSection = document.querySelector(".photographer_section");
 
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerTemplate(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    }
+  photographers.forEach((photographer) => {
+    const photographerModel = photographerTemplate(photographer);
+    const userCardDOM = photographerModel.getUserCardDOM();
+    photographersSection.appendChild(userCardDOM);
+  });
+}
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    }
-    
-    init();
-    
+async function init() {
+  // Récupère les datas des photographes
+  const { photographers } = await getPhotographers();
+  displayData(photographers);
+}
+
+init();
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,40 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+  const { name, id, city, country, tagline, price, portrait } = data;
+  const picture = `assets/photographers/${portrait}`;
 
-    const picture = `assets/photographers/${portrait}`;
-
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    img.setAttribute("alt", name);
+    img.setAttribute("aria-label", `Photographe: ${name}`);
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+    const h3 = document.createElement("h3");
+    h3.textContent = `${city}, ${country}`;
+    const p = document.createElement("p");
+    p.textContent = tagline;
+    const p2 = document.createElement("p");
+    p2.textContent = `${price}€/jour`;
+    p2.setAttribute("class", "price");
+    const a = document.createElement("a");
+    a.setAttribute("href", `photographer.html?id=${id}`);
+    a.appendChild(img);
+    a.appendChild(h2);
+    article.appendChild(a);
+    article.appendChild(h3);
+    article.appendChild(p);
+    article.appendChild(p2);
+    return article;
+  }
+  return {
+    name,
+    id,
+    city,
+    country,
+    tagline,
+    price,
+    picture,
+    getUserCardDOM,
+  };
 }
